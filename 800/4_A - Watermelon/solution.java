27b43920321900e513f1403edf491f090bd34495
Harshit Maurya <44harshitmaurya@gmail.com>
2026-07-29 23:25:06 +0530

    Add Watermelon [A] from Codeforces

---
+++ 800/4_A - Watermelon/solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ 
+public final class Watermelon{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int totalWeight = sc.nextInt();
+        
+        if(totalWeight % 2 == 0 && totalWeight > 2){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+        
+    }
+}
